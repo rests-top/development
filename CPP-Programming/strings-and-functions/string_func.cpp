897f61d913bc38658a0754dfe0828f793acf2bff
Brett Porter <apostatelabs@gmail.com>
2021-01-24 16:50:03 -0500

    adding code from tutorial

---
+++ CPP-Programming/strings-and-functions/string_func.cpp
@@ -0,0 +1,21 @@
+// start with header information
+// functions and data members for use in the program
+#include<iostream>
+#include<string>
+// below the double colon is the scope resolution operator
+// identifying the namespace
+// a namespace is a collection of functions and data members the format namespace::identifier
+using std::cout;
+using std::string;
+
+int main()
+{
+    //prompt for input
+    cout << "Please enter a phrase";
+    string phrase;
+    getline(std::cin, phrase);
+    
+
+
+
+}
